123120ac2cab5a5210c63729659411282afb673e
Maxime CARABINA <maxime.carabina@epitech.eu>
2022-02-16 18:05:05 +0100

    [A UP

--- First_Part/ex03.py
+++ First_Part/ex03.py
@@ -10,10 +10,12 @@ if __name__ == "__main__":
     res = 0
     test = ['4', '3', '-2']
 
+    if len(test) == 1:
+        print("calculate(", test, ") -> False")
     for i in range(len(test)):
         if test[i][0] == '-' and test[i][1].isdigit():
             res += int(test[i])
         elif test[i].isdigit():
             res += int(test[i])
-    print(res)
+    print("calculate(", test, ") -> ", res)
     exit(0)
